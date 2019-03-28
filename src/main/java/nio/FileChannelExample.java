e00397b97d600b1cf657b82eed4108bbce2c8216
TommorowIsBetter <841331749@qq.com>
2019-03-28 19:51:24 +0800

    Add the nio readFile() method. the method also consider the unicode problem.

---
+++ src/main/java/nio/FileChannelExample.java
@@ -0,0 +1,46 @@
+package nio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+import org.junit.Test;
+
+public class FileChannelExample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * 
+	 * @throws IOException
+	 * @Description 通过NIO读取文件里面的内容，并且也处理了中文乱码的问题
+	 */
+	@Test
+	public void readFile() throws IOException {
+		Charset charset = Charset.forName("UTF-8");
+		CharsetDecoder decoder = charset.newDecoder();
+		RandomAccessFile aFile = new RandomAccessFile(FileChannelExample.class.getClassLoader().getResource("nio/nio-data.txt").getPath(), "rw");
+	    FileChannel inChannel = aFile.getChannel();
+	    ByteBuffer buf = ByteBuffer.allocate(200);
+	    CharBuffer charBuffer = CharBuffer.allocate(200);
+	    int bytesRead = inChannel.read(buf);
+	    while (bytesRead != -1) {
+	      System.out.println("Read " + bytesRead);
+	      buf.flip();
+          decoder.decode(buf, charBuffer, false);
+          charBuffer.flip();
+          System.out.println(charBuffer);
+	      buf.clear();
+	      charBuffer.clear();
+	      bytesRead = inChannel.read(buf);
+	    }
+	    aFile.close();
+	}
+
+}
